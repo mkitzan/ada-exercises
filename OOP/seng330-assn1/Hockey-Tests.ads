88178b9767f07b28dfae66b9faed5ec2f7c7af30
Michael Kitzan <mkitzan45@gmail.com>
2018-09-21 18:59:09 -0700

    Tabs for 4 spaces

--- OOP/seng330-assn1/Hockey-Tests.ads
+++ OOP/seng330-assn1/Hockey-Tests.ads
@@ -3,20 +3,20 @@ with Hockey.League, Hockey.Team, Hockey.Player;
 use  Hockey.League, Hockey.Team, Hockey.Player;
 
 package Hockey.Tests is
-	procedure Test_Player_Constructor;
-	procedure Test_Team_Constructor;
-	procedure Test_League_Constructor;
-	
-	procedure Test_Team_Add;
-	procedure Test_League_Add;
-	
-	procedure Test_Team_Remove;
-	procedure Test_Team_Remove_Empty;
-	procedure Test_Team_Remove_Missing;
-	
-	procedure Test_League_Remove;
-	procedure Test_League_Remove_Empty;
-	procedure Test_League_Remove_Missing;
-	
-	procedure Test_All;
+    procedure Test_Player_Constructor;
+    procedure Test_Team_Constructor;
+    procedure Test_League_Constructor;
+    
+    procedure Test_Team_Add;
+    procedure Test_League_Add;
+    
+    procedure Test_Team_Remove;
+    procedure Test_Team_Remove_Empty;
+    procedure Test_Team_Remove_Missing;
+    
+    procedure Test_League_Remove;
+    procedure Test_League_Remove_Empty;
+    procedure Test_League_Remove_Missing;
+    
+    procedure Test_All;
 end Hockey.Tests;
--- OOP/seng330-assn1/Hockey.ads
+++ OOP/seng330-assn1/Hockey.ads
@@ -2,7 +2,7 @@ with Ada.Strings.Unbounded;
 use  Ada.Strings.Unbounded;
 
 package Hockey is
-	type Role is ( CENTER, GOALTENDER, WINGER, DEFENDER );
-	Max_Players : Positive := 22; -- Standard hockey team size
-	Max_Teams   : Positive := 32; -- 31 + Seattle Metropolitans
+    type Role is ( CENTER, GOALTENDER, WINGER, DEFENDER );
+    Max_Players : Positive := 22; -- Standard hockey team size
+    Max_Teams   : Positive := 32; -- 31 + Seattle Metropolitans
 end Hockey;
